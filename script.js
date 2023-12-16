2a26619ab5a17ecefb1619e4967bd7edb3c0ec9c
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-16 07:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+ let f = prompt("enter first variable");
+ let s = prompt("enter second variable");
+
+let output = 4*f + 2*s;
+alert("The final result is: " + output);
